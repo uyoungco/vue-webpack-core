a07cac7d534af70349b421c2879851014fa4eb82
uyoungco <uyoung.co@gmail.com>
2018-05-25 00:39:24 +0800

    Vuex之state和getters

--- client/app.vue
+++ client/app.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="cover"></div>
     <Header></Header>
-    <p>{{count}}</p>
+    <p>{{fullName}} {{counter}}</p>
     <router-link to="/app">app</router-link>
     <router-link to="/app/123">app123</router-link>
     <router-link to="/app/456">app456</router-link>
@@ -16,6 +16,10 @@
 </template>
 
 <script>
+import {
+  mapState,
+  mapGetters
+} from 'vuex'
 import Header from './layout/header.vue'
 import Footer from './layout/footer.jsx'
 // import Todo from './views/todo/todo.vue'
@@ -34,9 +38,16 @@ export default {
     }, 1000)
   },
   computed: {
-    count () {
-      return this.$store.state.count
-    }
+    ...mapState({
+      counter: (state) => state.count
+    }),
+    // count () {
+    //   return this.$store.state.count
+    // },
+    ...mapGetters(['fullName'])
+    // fullName () {
+    //   return this.$store.getters.fullName
+    // }
   }
 }
 </script>
--- client/store/store.js
+++ client/store/store.js
@@ -1,14 +1,13 @@
 import Vuex from 'vuex'
 
+import defaultState from './state/state'
+import mutations from './mutations/mutations'
+import getters from './getters/getters'
+
 export default () => {
   return new Vuex.Store({
-    state: {
-      count: 0
-    },
-    mutations: {
-      updateCount (state, num) {
-        state.count = num
-      }
-    }
+    state: defaultState,
+    mutations: mutations,
+    getters
   })
 }
